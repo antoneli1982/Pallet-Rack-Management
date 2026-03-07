62ed2f534ca3aa0bce9153cccd9e6173f83499b4
antoneli1982 <antoneli.engenheiro@gmail.com>
2026-03-07 12:51:20 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,8 @@
 const CACHE = 'trilium-5w2h-v1';
 const FILES = ['/', '/index.html', '/icon-192.png', '/icon-512.png', '/manifest.json'];
-
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
 });
-
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
