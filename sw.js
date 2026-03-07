ce8e37aa0953ebf1492e80a9fb4585b7349c2834
antoneli1982 <antoneli.engenheiro@gmail.com>
2026-03-07 15:53:20 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-const C='5w2h-v2';const F=['/','index.html','icon-192.png','icon-512.png','manifest.json'];
+const C='5w2h-v3';const F=['/','index.html','manifest.json','icon-192.png','icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(F))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
